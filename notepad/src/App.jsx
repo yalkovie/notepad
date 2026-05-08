44b28a97a77b6726d7dd143f1d2d2bc94e8ab926
Ilya <yalkovie@mail.ru>
2026-05-08 19:24:32 +0300

    style

--- notepad/src/App.jsx
+++ notepad/src/App.jsx
@@ -14,6 +14,9 @@ function App() {
   let [modalTask, setModalTask] = useState(null)
   let [deleteTask, setDeleteTask] = useState(null)
 
+  let [startDate, setStartDate] = useState('')
+  let [endDate, setEndDate] = useState('')
+
   useEffect(() => {
     localStorage.setItem('localTasks', JSON.stringify(tasks))
   }, [tasks])
@@ -35,13 +38,13 @@ function App() {
     return tasks;
   }
 
-  function onDelete (id) {
+  function onDelete(id) {
     setTasks(tasks.filter((task) => task.id !== id))
     setDeleteIsOpen(false)
     console.log(tasks);
   }
 
-  function confirmDelete (task) {
+  function confirmDelete(task) {
     setDeleteTask(task)
     setDeleteIsOpen(true)
   }
@@ -55,6 +58,50 @@ function App() {
     setIsOpen(true)
   }
 
+  function change_st(id, taskState) {
+    setTasks(tasks.map((el) => (el.id === id ? {...el, state : taskState} : el)))
+  }
+
+  function filterTask(tasks) {
+    if (startDate && endDate) {
+      let firstDate = new Date(startDate)
+      let secondDate = new Date(endDate)
+      return tasks.filter(el => {
+        let task_date = new Date(el.date)
+      return  task_date >= firstDate && task_date <= secondDate})
+    }
+    return tasks
+  }
+
+  function pars_date(date) {
+    if (!date) {
+      return ''
+    }
+    let [y, m ,d] = date.split('-')
+    if (y[0] != 0) {
+      return true
+    }
+    return false
+  }
+
+  function start_handle_change (el) {
+    console.log(el);
+    let value = el.target.value
+    console.log(value);
+    console.log(value[0]);
+    if (value === '' || value.length === 10) {
+      if (pars_date(value)) {
+        setStartDate(value)
+      }
+    }
+  }
+
+  function end_handle_change (el) {
+    let value = el.target.value
+    setEndDate(value)
+    
+  }
+
   return (
     <>
       <div id='app'>
@@ -62,16 +109,17 @@ function App() {
           <button className='header_btn' onClick={newTask}>НОВАЯ ЗАДАЧА</button>
           <div className='filter'>
             <span>Фильтр по дедлайнам с:</span>
-            <input type="date" name="firstDat" id="firstDat" />
+            <input type="date" name="firstDat" id="firstDat" value={startDate || ''} onChange={start_handle_change}/>
             <span>по:</span>
-            <input type="date" name="secondDat" id="secondDat" />
+            <input type="date" name="secondDat" id="secondDat" value={endDate || ''} onChange={end_handle_change}/>
             <button  className='header_btn'>СБРОСИТЬ ФИЛЬТРЫ</button>
           </div>
         </header>
-
-        <Window title={'ПОСТАВЛЕННЫЕ'} backgroundColor={'#ff99ff'} onEdit={onEdit} tasks={tasks.filter((task) => task.state === 'Поставленная')} onDelete={() => {confirmDelete(deleteTask)}} /*change_st={None}*//>
-        <Window title={'В РАБОТЕ'} backgroundColor={'#99ffff'} onEdit={onEdit} tasks={tasks.filter((task) => task.state === 'В работе')} onDelete={() => {confirmDelete(deleteTask)}} /*change_st={None}*//>
-        <Window title={'ВЫПОЛНЕННЫЕ'} backgroundColor={'#99ff99'} onEdit={onEdit} tasks={tasks.filter((task) => task.state === 'Выполненная')} onDelete={() => {confirmDelete(deleteTask)}} /* change_st={None}*//>
+        <div id='window'>
+          <Window className='wind' title={'ПОСТАВЛЕННЫЕ'} backgroundColor={'#ff99ff'} onEdit={onEdit} tasks={filterTask(tasks).filter((task) => task.state === 'Поставленная')} onDelete={(deleteTask) => {confirmDelete(deleteTask)}} change_st={change_st}/>
+          <Window className='wind' title={'В РАБОТЕ'} backgroundColor={'#99ffff'} onEdit={onEdit} tasks={filterTask(tasks).filter((task) => task.state === 'В работе')} onDelete={(deleteTask) => {confirmDelete(deleteTask)}} change_st={change_st}/>
+          <Window className='wind' title={'ВЫПОЛНЕННЫЕ'} backgroundColor={'#99ff99'} onEdit={onEdit} tasks={filterTask(tasks).filter((task) => task.state === 'Выполненная')} onDelete={(deleteTask) => {confirmDelete(deleteTask)}} change_st={change_st}/>
+        </div>
       </div>
 
       <ModalNew task={modalTask} isOpen={isOpen} onClose={onClose} onSave={onSave}/>
--- notepad/src/components/Card/Card.jsx
+++ notepad/src/components/Card/Card.jsx
@@ -1,24 +1,29 @@
 import './Card.less'
 import Button from '../Button/Button.jsx'
 
-function Card({task, onEdit, onDelete /*change_st*/}) {
+function Card({task, onEdit, onDelete, change_st}) {
+
+    let isLate = new Date(task.date) < new Date() && task.state !== 'Выполненная'
+
     return (
         <>
-        <header> 
-            <h1>{task.title}</h1>
-            <button onClick={() => {onDelete(task)}} className='delete'>x</button>
-        </header>
+        <div className='card' style={{backgroundColor: isLate ? 'red' : 'white', color: isLate ? 'white' : 'black'}}>
+            <header> 
+                <h1>{task.title}</h1>
+                <button onClick={() => {onDelete(task)}} className='delete'>x</button>
+            </header>
 
-        <p>{task.desc}</p> 
+            <p>{task.desc}</p> 
 
-        <div>
-            <p>Дедлайн: <span>{task.date}</span></p>
-        </div>
+            <div>
+                <p>Дедлайн: <span>{task.date}</span></p>
+            </div>
 
-        <div>
-            <Button /*onClick={change_st}*/ className='arrow' txt={'<'}/>
-            <Button onClick={() => (onEdit(task))} className='arrow' txt={'Редактировать'}/>
-            <Button /*onClick={change_st}*/ className='arrow' txt={'>'}/>
+            <div>
+                <Button onClick={() => (change_st(task.id, task.state === 'Выполненная' ? 'В работе' : 'Поставленная'))} className='arrow' txt={'<'}/>
+                <Button onClick={() => (onEdit(task))} className='arrow' txt={'Редактировать'}/>
+                <Button onClick={() => (change_st(task.id, task.state === 'Поставленная' ? 'В работе' : 'Выполненная'))} className='arrow' txt={'>'}/>
+            </div>
         </div>
         </>
     )
